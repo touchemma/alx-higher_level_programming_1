45447c513a1a8402fc170238adb598d57b5928d3
touchemma <emmagithub1@gmail.com>
2023-03-11 20:39:07 -0800

    task

--- 0x03-Python-Data_Structures/lists.h
+++ 0x03-Python-Data_Structures/lists.h
@@ -20,5 +20,16 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
+def print_list_integer(my_list=[]);
+def element_at(my_list, idx);
+def replace_in_list(my_list, idx, element);
+def print_reversed_list_integer(my_list=[]);
+def new_in_list(my_list, idx, element);
+def no_c(my_string);
+def print_matrix_integer(matrix=[[]]);
+def add_tuple(tuple_a=(), tuple_b=());
+def multiple_returns(sentence);
+def max_integer(my_list=[]);
+def divisible_by_2(my_list=[]);
 
 #endif
