a7722d4f309059268ececb31fbdf147d92ad5fca
touchemma <emmagithub1@gmail.com>
2023-03-12 18:02:40 -0700

    task update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,10 +5,3 @@ def delete_at(my_list=[], idx=0):
     else:
         del my_list[idx]
     return my_list
-
-12-switch.py
-#!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
