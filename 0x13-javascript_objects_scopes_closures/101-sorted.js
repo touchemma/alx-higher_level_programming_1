42749c619cd54378492cfc944cf90d655c2f9fda
touchemma <emmagithub1@gmail.com>
2023-06-15 05:53:44 -0700

    javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,7 @@
+const dict = require('./101-data').dict;
+const newDict = Object.entries(dict).reduce((item, [k, v]) => {
+  item[v] = item[v] ? [...item[v], k] : [k];
+  return item;
+}, {});
+
+console.log(newDict);
